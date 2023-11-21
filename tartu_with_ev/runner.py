750351dcf4d52e97a3cd82f245f0ba0dffdc5043
kuchidareo <kuchidareo@gmail.com>
2023-11-21 16:19:55 +0200

    Add a function to go to a nearest station.

--- tartu_with_ev/runner.py
+++ tartu_with_ev/runner.py
@@ -3,26 +3,63 @@ import sys
 if 'SUMO_HOME' in os.environ:
     sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
 import traci
-
-from sumolib import checkBinary  # noqa
+import gzip
+import math
+from sumolib import checkBinary
 import xml.etree.ElementTree as ET
 
-sumoBinary = checkBinary("sumo")
-# sumoBinary = checkBinary('sumo-gui') # to watch the simulation on sumo-gui
-sumoCmd = [sumoBinary, "-c", "osm.sumocfg", "--no-warnings"]
-
-def getIds_EV():
-    # Parse the XML file
-    tree = ET.parse('car.ev.trips.xml')
+def getIds_EV(xml):
+    tree = ET.parse(xml)
     root = tree.getroot()
     # Find elements with 'type' attribute equal to 'soulEV65'
     soulEV65_ids = [elem.get('id') for elem in root.findall(".//trip[@type='soulEV65']")]
     return soulEV65_ids
 
-traci.start(sumoCmd)
-soulEV65_ids = getIds_EV()
+def get_chargingstations(xml_gz):
+    with gzip.open(xml_gz, 'rt') as xml:
+        tree = ET.parse(xml)
+    root = tree.getroot()
+
+    charging_stations = []
+    for charging_station in root.iter('chargingStation'):
+        station_id = charging_station.get('id')
+        station_lane = charging_station.get('lane') # 29394205#0_0"
+        station_edge = station_lane.split("_")[0] # 29394205#0"
+        charging_stations.append({"id": station_id, "lane": station_lane, "edge": station_edge})
+
+    return charging_stations
+
+def find_nearest_charging_station(ev_id, charging_stations):
+    vehicle_edge_id = traci.vehicle.getRoadID(ev_id)
+    min_distance = float("inf")
+    for station in charging_stations:
+        station_id = station["id"]
+        station_edge_id = station["edge"]
+        distance = traci.simulation.findRoute(vehicle_edge_id, station_edge_id).length
+
+        if distance < min_distance:
+            min_distance = distance
+            nearest_station_id = station_id
+            nearest_station_edge_id = station_edge_id
+
+    return nearest_station_id, nearest_station_edge_id
+
+
+# sumoBinary = checkBinary("sumo")
+sumoBinary = checkBinary('sumo-gui') # to watch the simulation on sumo-gui
+sumoCmd = [sumoBinary, "-c", "osm.sumocfg", "--no-warnings"]
+
+EV_trip_xml = "car.ev.trips.xml"
+charging_station_xml_gz = "charging.stations.xml.gz"
+
+soulEV65_ids = getIds_EV(EV_trip_xml)
+charging_stations = get_chargingstations(charging_station_xml_gz)
+charging_duration = 100
 
 simulation_step = 4000 # the last car departs at step 3500.
+
+traci.start(sumoCmd)
+
 step = 0
 while step < simulation_step:
     if step % 100 == 0:
@@ -37,16 +74,15 @@ while step < simulation_step:
             battery_remain = float(traci.vehicle.getParameter(ev_id, "device.battery.actualBatteryCapacity"))
             battery_capacity = float(traci.vehicle.getParameter(ev_id, "device.battery.maximumBatteryCapacity"))
             if battery_remain < battery_capacity / 10:
-                # TODO: Find the nearest charging station and go
                 # TODO: Add charging stations
+                # TODO: Fix the error that there is no route to the station.
                 # TODO: Set the natual parameters of charging stations.
-                delta_charging_station_id = "cS_2to19_0a"
-                delta_charging_station_edge_id = "29394205#0"
-                charging_duration = 100
-                traci.vehicle.changeTarget(ev_id, delta_charging_station_edge_id)
-                traci.vehicle.setChargingStationStop(ev_id, delta_charging_station_id, duration=charging_duration)
+                nearest_station_id, nearest_station_edge_id = find_nearest_charging_station(ev_id, charging_stations)
+                print(f"vehicle:{ev_id} is going to charging station:{nearest_station_id}")
+                traci.vehicle.changeTarget(ev_id, nearest_station_edge_id)
+                traci.vehicle.setChargingStationStop(ev_id, nearest_station_id, duration=charging_duration)
                 # traci.vehicle.setParameter(ev_id, "device.battery.actualBatteryCapacity", str(battery_capacity))
-                # print(f"vehicle:{ev_id} is going to charging station:{delta_charging_station_id}")
+       
         except traci.exceptions.TraCIException as e: # Not Found the ev in the simulation map
             continue
     step += 1
