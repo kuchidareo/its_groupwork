86488e5c824db3deb4cce264cd907fe5199d3250
ritudish <ritudisha.biswas@ut.ee>
2023-11-29 23:46:44 +0200

    working of queue tested

--- tartu_with_ev/runner.py
+++ tartu_with_ev/runner.py
@@ -8,6 +8,15 @@ import math
 from sumolib import checkBinary
 import xml.etree.ElementTree as ET
 
+class waiting_queue_item:
+    def __init__(self, charging_station_id, id, waiting_step_counter, charging_step_counter):
+        self.charging_station_id = charging_station_id
+        self.id = id
+        self.waiting_step_counter = waiting_step_counter
+        self.charging_step_counter = charging_step_counter
+
+
+
 def getIds_EV(xml):
     tree = ET.parse(xml)
     root = tree.getroot()
@@ -45,8 +54,8 @@ def find_nearest_charging_station(ev_id, charging_stations):
     return nearest_station_id, nearest_station_edge_id
 
 
-# sumoBinary = checkBinary("sumo")
-sumoBinary = checkBinary('sumo-gui') # to watch the simulation on sumo-gui
+sumoBinary = checkBinary("sumo")
+# sumoBinary = checkBinary('sumo-gui') # to watch the simulation on sumo-gui
 sumoCmd = [sumoBinary, "-c", "osm.sumocfg", "--no-warnings"]
 
 EV_trip_xml = "car.ev.trips.xml"
@@ -60,6 +69,8 @@ simulation_step = 4000 # the last car departs at step 3500.
 
 traci.start(sumoCmd)
 
+waiting_queues = {}
+ev_station = {}
 step = 0
 while step < simulation_step:
     if step % 100 == 0:
@@ -73,12 +84,31 @@ while step < simulation_step:
         try:
             battery_remain = float(traci.vehicle.getParameter(ev_id, "device.battery.actualBatteryCapacity"))
             battery_capacity = float(traci.vehicle.getParameter(ev_id, "device.battery.maximumBatteryCapacity"))
+            
+            if(ev_id in ev_station.keys()):
+                queue = waiting_queues[ev_station[ev_id]]
+                if(queue[0].charging_step_counter == 138):
+                    queue.pop(0) 
+                if(len(queue) > 1 and queue[1].charging_step_counter == 138):
+                    queue.pop(1) 
+
+                for (item, idx) in zip(queue, range(len(queue))):
+                    if(idx == 0 or idx == 1):
+                        queue[idx].charging_step_counter = queue[idx].charging_step_counter + 1
+                    else:
+                        queue[idx].waiting_step_counter = queue[idx].waiting_step_counter + 1
+
             if battery_remain < battery_capacity / 10:
                 # TODO: Add charging stations
                 # TODO: Fix the error that there is no route to the station.
                 # TODO: Set the natual parameters of charging stations.
                 nearest_station_id, nearest_station_edge_id = find_nearest_charging_station(ev_id, charging_stations)
-                print(f"vehicle:{ev_id} is going to charging station:{nearest_station_id}")
+                # print(f"vehicle:{ev_id} is going to charging station:{nearest_station_id}")
+                if(nearest_station_id not in waiting_queues.keys()):
+                    waiting_queues[nearest_station_id] = [] #initializing queue for the charging station
+
+                waiting_queues[nearest_station_id].append(waiting_queue_item(nearest_station_id, ev_id, 0, 0))
+                ev_station[ev_id] = nearest_station_id
                 traci.vehicle.changeTarget(ev_id, nearest_station_edge_id)
                 traci.vehicle.setChargingStationStop(ev_id, nearest_station_id, duration=charging_duration)
                 # traci.vehicle.setParameter(ev_id, "device.battery.actualBatteryCapacity", str(battery_capacity))
